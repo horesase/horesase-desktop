f5cdf918d62a28fe0b2b6b7ac6cf2c80fc0ad187
june29 <june29.jp@gmail.com>
2015-11-08 18:38:11 +0900

    Basic view, and character list

--- build/javascripts/app.js
+++ build/javascripts/app.js
@@ -27,14 +27,104 @@ var MeigenList = React.createClass({displayName: "MeigenList",
     });
 
     return(
-      React.createElement("ul", {id: "meigen-list"}, 
+      React.createElement("ul", {id: "meigen-list"}, list)
+    );
+  }
+});
+
+var Character = React.createClass({displayName: "Character",
+  propTypes: {
+    id:              React.PropTypes.number.isRequired,
+    name:            React.PropTypes.string.isRequired,
+    selected:        React.PropTypes.bool.isRequired,
+    selectCharacter: React.PropTypes.func.isRequired
+  },
+
+  onClick:function() {
+    this.props.selectCharacter(this.props.id);
+  },
+
+  render:function() {
+    if (this.props.selected) {
+      return React.createElement("li", {className: "character selected"}, this.props.name)
+    } else {
+      return React.createElement("li", {className: "character", onClick: this.onClick}, this.props.name)
+    }
+  }
+});
+
+var CharacterList = React.createClass({displayName: "CharacterList",
+  propTypes: {
+    characters:         React.PropTypes.array.isRequired,
+    currentCharacterID: React.PropTypes.number.isRequired,
+    selectCharacter:    React.PropTypes.func.isRequired
+  },
+
+  render:function() {
+    var list = this.props.characters.map(function(c)  {
+      if (c.id == this.props.currentCharacterID) {
+        return React.createElement(Character, {id: c.id, name: c.name, key: c.id, selected: true, selectCharacter: this.props.selectCharacter})
+      } else {
+        return React.createElement(Character, {id: c.id, name: c.name, key: c.id, selected: false, selectCharacter: this.props.selectCharacter})
+      }
+    }.bind(this));
+
+    if (this.props.currentCharacterID == 0) {
+      list.unshift(React.createElement(Character, {id: 0, name: "すべてのキャラクター", key: 0, selected: true, selectCharacter: this.props.selectCharacter}));
+    } else {
+      list.unshift(React.createElement(Character, {id: 0, name: "すべてのキャラクター", key: 0, selected: false, selectCharacter: this.props.selectCharacter}));
+    }
+
+    return(
+      React.createElement("ul", {id: "character-list"}, 
         list
       )
     );
   }
 });
 
+var Horesase = React.createClass({displayName: "Horesase",
+  propTypes: {
+    meigens: React.PropTypes.array.isRequired
+  },
+
+  getInitialState:function() {
+    return {
+      currentCharacterID: 0,
+      displayedMeigens:   _.take(this.props.meigens.reverse(), 36)
+    }
+  },
+
+  selectCharacter:function(characterID) {
+    var _displayedMeigens;
+
+    if (characterID == 0) {
+      _displayedMeigens = _.take(this.props.meigens, 36)
+    } else {
+      _displayedMeigens = _.take(this.props.meigens.filter(function(m)  { return m.cid == characterID }), 36)
+    }
+
+    this.setState({
+      currentCharacterID: characterID,
+      displayedMeigens:   _displayedMeigens
+    });
+  },
+
+  render:function() {
+    var characters = _.uniq(this.props.meigens.map(function(m)  {
+      return { id: m.cid, name: m.character }
+    }), "id");
+
+    return(
+      React.createElement("div", {id: "horesase"}, 
+        React.createElement(CharacterList, {characters: characters, currentCharacterID: this.state.currentCharacterID, selectCharacter: this.selectCharacter}), 
+        React.createElement(MeigenList, {meigens: this.state.displayedMeigens})
+      )
+    );
+  }
+});
+
 ReactDOM.render(
-  React.createElement(MeigenList, {meigens: meigens}),
-  document.getElementById("main")
+  React.createElement(Horesase, {meigens: meigens}),
+  document.getElementById("app")
 );
--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -27,14 +27,104 @@ var MeigenList = React.createClass({
     });
 
     return(
-      <ul id="meigen-list">
+      <ul id="meigen-list">{list}</ul>
+    );
+  }
+});
+
+var Character = React.createClass({
+  propTypes: {
+    id:              React.PropTypes.number.isRequired,
+    name:            React.PropTypes.string.isRequired,
+    selected:        React.PropTypes.bool.isRequired,
+    selectCharacter: React.PropTypes.func.isRequired
+  },
+
+  onClick() {
+    this.props.selectCharacter(this.props.id);
+  },
+
+  render() {
+    if (this.props.selected) {
+      return <li className="character selected">{this.props.name}</li>
+    } else {
+      return <li className="character" onClick={this.onClick}>{this.props.name}</li>
+    }
+  }
+});
+
+var CharacterList = React.createClass({
+  propTypes: {
+    characters:         React.PropTypes.array.isRequired,
+    currentCharacterID: React.PropTypes.number.isRequired,
+    selectCharacter:    React.PropTypes.func.isRequired
+  },
+
+  render() {
+    var list = this.props.characters.map((c) => {
+      if (c.id == this.props.currentCharacterID) {
+        return <Character id={c.id} name={c.name} key={c.id} selected={true} selectCharacter={this.props.selectCharacter} />
+      } else {
+        return <Character id={c.id} name={c.name} key={c.id} selected={false} selectCharacter={this.props.selectCharacter} />
+      }
+    });
+
+    if (this.props.currentCharacterID == 0) {
+      list.unshift(<Character id={0} name="すべてのキャラクター" key={0} selected={true} selectCharacter={this.props.selectCharacter} />);
+    } else {
+      list.unshift(<Character id={0} name="すべてのキャラクター" key={0} selected={false} selectCharacter={this.props.selectCharacter} />);
+    }
+
+    return(
+      <ul id="character-list">
         {list}
       </ul>
     );
   }
 });
 
+var Horesase = React.createClass({
+  propTypes: {
+    meigens: React.PropTypes.array.isRequired
+  },
+
+  getInitialState() {
+    return {
+      currentCharacterID: 0,
+      displayedMeigens:   _.take(this.props.meigens.reverse(), 36)
+    }
+  },
+
+  selectCharacter(characterID) {
+    var _displayedMeigens;
+
+    if (characterID == 0) {
+      _displayedMeigens = _.take(this.props.meigens, 36)
+    } else {
+      _displayedMeigens = _.take(this.props.meigens.filter((m) => { return m.cid == characterID }), 36)
+    }
+
+    this.setState({
+      currentCharacterID: characterID,
+      displayedMeigens:   _displayedMeigens
+    });
+  },
+
+  render() {
+    var characters = _.uniq(this.props.meigens.map((m) => {
+      return { id: m.cid, name: m.character }
+    }), "id");
+
+    return(
+      <div id="horesase">
+        <CharacterList characters={characters} currentCharacterID={this.state.currentCharacterID} selectCharacter={this.selectCharacter} />
+        <MeigenList meigens={this.state.displayedMeigens} />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <MeigenList meigens={meigens} />,
-  document.getElementById("main")
+  <Horesase meigens={meigens} />,
+  document.getElementById("app")
 );
